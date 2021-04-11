9a2c295945fa20835634e4d148210476fe5bc0ca
csrinesh <csrinesh@gmail.com>
2021-04-11 16:32:18 +0800

    Linear regression module updated

--- linear_regression.py
+++ linear_regression.py
@@ -1,8 +1,11 @@
 import csv
-from matrix_ops import matmul, getTranspose, getMatrixInverse
+from matrix_ops import *
+from matrix_ops import myArray as my
 
 x_vars = ['x1', 'x2', 'x3']
 y_vars = ['y']
+
+fit_intercept = True
 
 with open('datasets\lin_reg.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -13,13 +16,28 @@ with open('datasets\lin_reg.csv') as csv_file:
     x_indices = [i for i, j in enumerate(header) if j in x_vars]
     y_indices = [i for i, j in enumerate(header) if j in y_vars]
     
-    x = []
-    y = []
-
+    x, y = [], []
+    
     for row in csv_reader:
         r = row[0].split(',')
-        x.append([float(j) for i, j in enumerate(r) if i in x_indices])
-        y.append([float(j) for i, j in enumerate(r) if i in y_indices])
+        read_row = [float(j) for i, j in enumerate(r) if i in x_indices]
 
-    alpha = matmul(matmul(getMatrixInverse(matmul(getTranspose(x), x)), getTranspose(x)), y)
-    print(alpha)
+        if fit_intercept:
+            read_row.extend([1])
+        
+        x.append(read_row)
+        y.append([float(j) for i, j in enumerate(r) if i in y_indices])
+    
+    x = myArray(x)
+    y = myArray(y)
+
+    z = x.t()*x
+    xtx_inv = myArray.getMatrixInverse(z.array)
+    alpha = (xtx_inv*x.t())*y
+    
+    if fit_intercept:
+        print('Estimated slopes of the Linear Regression:', *[i[0] for i in alpha.array[:-1]])
+        print('Estimated intercept of the Linear Regression:', *alpha.array[-1])
+    else:
+        print('Estimated slopes of the Linear Regression:',  *[i[0] for i in alpha.array])
+
--- matrix_ops.py
+++ matrix_ops.py
@@ -1,52 +1,103 @@
-from itertools import starmap
-from operator import mul
+from operator import mul, add
 
-def matmul(a, b):
-    # Check if all rows are of same length
-    for i in [a, b]:
-        it = iter(i)
-        the_len = len(next(it))
-        if not all(len(l) == the_len for l in it):
-            raise ValueError('not all lists have same length!')
+class myArray(object):
+    def __init__(self, array):
+        self.array = array
+        self.shape = myArray.get_shape(self.array)
+
+    @staticmethod
+    def get_shape(obj, shape=()):
+        if not isinstance(obj, list):
+            return shape
+        
+        shape += (len(obj), )
+
+        if isinstance(obj[0], list):
+            if not all(len(item) == len(obj[0]) for item in obj):
+                raise ValueError('All intra lists do not have shape length')
+        shape = myArray.get_shape(obj[0], shape=shape)
+
+        return shape
     
-    # Check if matrix compatible
-    if len(a[0]) != len(b):
-        raise ValueError('not multiplication compatible')
+    def __repr__(self):
+        return f'myArray{self.array}'
+
+    def __add__(self, other):
+        if len(self.shape) > 2:
+            print('Only 2d arrays are supported')
+            raise NotImplemented
+
+        if self.shape == other.shape:
+            return [list(map(lambda x, y: x+y, self.array[i] ,
+             other.array[i])) for i in range(self.shape[:-1][0])]
+        else:
+            raise 'shape of the arrays incompatible for addition'
+
+    def __sub__(self, other):
+        if len(self.shape) > 2:
+            print('Only 2d arrays are supported')
+            raise NotImplemented
+
+        if self.shape == other.shape:
+            return [list(map(lambda x, y: x-y, self.array[i] ,
+             other.array[i])) for i in range(self.shape[:-1][0])]
+        else:
+            raise 'shape of the arrays incompatible for subtraction'
+
+    def __mul__(self, other):
+        a, b = self.array, other.array
+        for i in [a, b]:
+            it = iter(i)
+            the_len = len(next(it))
+            if not all(len(l) == the_len for l in it):
+                raise ValueError('not all lists have same length!')
+        
+        # Check if matrix compatible
+        if len(a[0]) != len(b):
+            raise ValueError('not multiplication compatible')
+        
+        return myArray([[sum(map(mul, row, col)) for col in zip(*b)] for row in a])
     
-    return [[sum(map(mul, row, col)) for col in zip(*b)] for row in a]
+    def t(self):
+        x = self.array
+        return myArray(list(map(list, zip(*x))))
 
-def getTranspose(x):
-    return list(map(list, zip(*x)))
+    @staticmethod
+    def getTranspose(x):
+        return list(map(list, zip(*x)))
 
-def getMatrixMinor(x,i,j):
-    return [row[:j] + row[j+1:] for row in (x[:i]+x[i+1:])]
+    @staticmethod
+    def getMatrixInverse(x):
+        determinant = myArray.getMatrixDeterminant(x)
+        #special case for 2x2 matrix:
+        if len(x) == 2:
+            return [[x[1][1]/determinant, -1*x[0][1]/determinant],
+                    [-1*x[1][0]/determinant, x[0][0]/determinant]]
 
-def getMatrixDeternminant(x):
-    if len(x) == 2:
-        return x[0][0]*x[1][1]-x[0][1]*x[1][0]
+        #find matrix of cofactors
+        cofactors = []
+        for r in range(len(x)):
+            cofactorRow = []
+            for c in range(len(x)):
+                minor = myArray.getMatrixMinor(x,r,c)
+                cofactorRow.append(((-1)**(r+c)) * myArray.getMatrixDeterminant(minor))
+            cofactors.append(cofactorRow)
+        cofactors = myArray.getTranspose(cofactors)
+        for r in range(len(cofactors)):
+            for c in range(len(cofactors)):
+                cofactors[r][c] = cofactors[r][c]/determinant
+        return myArray(cofactors)
 
-    determinant = 0
-    for c in range(len(x)):
-        determinant += ((-1)**c)*x[0][c]*getMatrixDeternminant(getMatrixMinor(x,0,c))
-    return determinant
+    @staticmethod
+    def getMatrixMinor(x,i,j):
+        return [row[:j] + row[j+1:] for row in (x[:i]+x[i+1:])]
 
-def getMatrixInverse(x):
-    determinant = getMatrixDeternminant(x)
-    #special case for 2x2 matrix:
-    if len(x) == 2:
-        return [[x[1][1]/determinant, -1*x[0][1]/determinant],
-                [-1*x[1][0]/determinant, x[0][0]/determinant]]
+    @staticmethod
+    def getMatrixDeterminant(x):
+        if len(x) == 2:
+            return x[0][0]*x[1][1]-x[0][1]*x[1][0]
 
-    #find matrix of cofactors
-    cofactors = []
-    for r in range(len(x)):
-        cofactorRow = []
+        determinant = 0
         for c in range(len(x)):
-            minor = getMatrixMinor(x,r,c)
-            cofactorRow.append(((-1)**(r+c)) * getMatrixDeternminant(minor))
-        cofactors.append(cofactorRow)
-    cofactors = getTranspose(cofactors)
-    for r in range(len(cofactors)):
-        for c in range(len(cofactors)):
-            cofactors[r][c] = cofactors[r][c]/determinant
-    return cofactors
+            determinant += ((-1)**c)*x[0][c]*myArray.getMatrixDeterminant(myArray.getMatrixMinor(x,0,c))
+        return determinant
